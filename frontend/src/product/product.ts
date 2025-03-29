b38f4656836331cc1cf74c96aabe562e73c3ed74
Volper212 <volpergaming@gmail.com>
2025-03-29 18:29:39 +0100

    fix

--- frontend/src/product/product.ts
+++ frontend/src/product/product.ts
@@ -25,26 +25,12 @@ export type Product = {
   costScore?: number;
 };
 
-// export const energyPricePerKwh = 1.11;
-// export const waterPricePerL = 0.01;
-// export const energyCo2EmissionsPerKwh = 652;
-// export const waterCo2EmissionsPerL = 0.0106;
-// export type UserPreference = 1 | 2 | 3 | 4 | 5;
-// export const defaultUserPreference: UserPreference = 3;
-
-// export const packagingEnvironmentalImpact = {
-//     plastic: 3.5,
-//     paper: 1.2,
-//     glass: 2.0,
-//     metal: 2.8
-//   };
-
 export const calculateEcoScore = (product: Product): number => {
   let score = 100;
   
   if (product.energyUsePerYear !== undefined) {
     const energyCO2 = product.energyUsePerYear * energyCo2EmissionsPerKwh;
-    score -= (energyCO2 / 10000);
+    score -= (energyCO2 / 100);
   }
   
   if (product.waterUsePerYear !== undefined) { // CO2 emissions from water use
