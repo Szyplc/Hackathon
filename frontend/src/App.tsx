cc74c89204cd7914308e2d27a7a0f6531926d3ed
Weronika Zygis <115404427+werka-z@users.noreply.github.com>
2025-03-29 17:39:10 +0100

    App theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import { Products } from "./components/Products";
 import Sidebar from "./components/Sidebar";
 import { useState } from "react";
+import AppTheme from './shared-theme/AppTheme'
 
 function App() {
     const [economicValue, setEconomicValue] = useState(50);
@@ -11,25 +12,27 @@ function App() {
     );
 
     return (
-        <Grid container>
-            <Grid size={2}>
-                <Sidebar
-                    economicValue={economicValue}
-                    setEconomicValue={setEconomicValue}
-                    ecologicValue={ecologicValue}
-                    setEcologicValue={setEcologicValue}
-                    selectedCategory={selectedCategory}
-                    setSelectedCategory={setSelectedCategory}
-                />
-            </Grid>
-            <Grid size={10}>
-                <Products
-                    economicValue={economicValue}
-                    ecologicValue={ecologicValue}
-                    selectedCategory={selectedCategory}
-                />
-            </Grid>
-        </Grid>
+        <AppTheme>
+                <Grid container>
+                    <Grid size={2}>
+                        <Sidebar
+                            economicValue={economicValue}
+                            setEconomicValue={setEconomicValue}
+                            ecologicValue={ecologicValue}
+                            setEcologicValue={setEcologicValue}
+                            selectedCategory={selectedCategory}
+                            setSelectedCategory={setSelectedCategory}
+                        />
+                    </Grid>
+                    <Grid size={10}>
+                        <Products
+                            economicValue={economicValue}
+                            ecologicValue={ecologicValue}
+                            selectedCategory={selectedCategory}
+                        />
+                    </Grid>
+                </Grid>
+        </AppTheme>
     );
 }
 
